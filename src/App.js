4994fad417efe9cce845931eba08f87a5fb960e2
gastighost <78007783+gastighost@users.noreply.github.com>
2021-09-01 12:57:25 +0800

    load user feature

--- src/App.js
+++ src/App.js
@@ -35,8 +35,26 @@ class App extends Component {
       imageUrl:'',
       box: {},
       route: 'SignIn',
-      isSignedIn: false
+      isSignedIn: false,
+      user: {
+        id: '',
+        name: '',
+        email: '',
+        entries: 0,
+        joined: ''
+      }
     }
+  };
+
+  loadUser = (data) => {
+    this.setState({user:{
+        id: data.id,
+        name: data.name,
+        email: data.email,
+        entries: data.entries,
+        joined: data.joined
+      }
+    })
   };
 
   calculateFaceLocation = (data) => {
@@ -91,15 +109,15 @@ class App extends Component {
       ? 
       <div>
       <Logo />
-      <Rank />
+      <Rank name={this.state.user.name} entries={this.state.user.entries}/>
       <ImageLinkForm onInputChange={this.onInputChange} onSubmit={this.onSubmit}/>
       <FaceRecognition imageUrl={imageUrl} box={box}/>
       </div>
       : (
         route === 'SignIn' 
-        ? <SignIn onRouteChange={this.onRouteChange}/>
+        ? <SignIn onRouteChange={this.onRouteChange} loadUser={this.loadUser}/>
         :
-          <Register onRouteChange={this.onRouteChange}/>
+          <Register onRouteChange={this.onRouteChange} loadUser={this.loadUser}/>
       )
 
       }
