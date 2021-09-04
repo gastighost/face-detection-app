f953fd8b101bb39447dc940f0412b14fbe68d55e
gastighost <78007783+gastighost@users.noreply.github.com>
2021-09-04 14:07:49 +0800

    fixed connection

--- src/App.js
+++ src/App.js
@@ -89,7 +89,7 @@ class App extends Component {
     app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
     .then(response => {
       if (response) {
-        fetch('http://localhost:3000/image', {
+        fetch('https://warm-peak-60310.herokuapp.com/:3000/image', {
           method: 'put',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({
